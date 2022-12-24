1171666bfda23fa1ff24f0f5ec6ae7367fe62523
Frank Rocha <contato.frankrocha@gmail.com>
2022-12-24 11:15:52 -0300

    feat: Add syntax code to editor

--- src/renderer/src/components/Editor/index.tsx
+++ src/renderer/src/components/Editor/index.tsx
@@ -1,9 +1,21 @@
+import '../../styles/codeEditor.scss'
 import Document from '@tiptap/extension-document'
 import Highlight from '@tiptap/extension-highlight'
 import Placeholder from '@tiptap/extension-placeholder'
 import Typography from '@tiptap/extension-typography'
 import { EditorContent, useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import css from 'highlight.js/lib/languages/css'
+import js from 'highlight.js/lib/languages/javascript'
+import ts from 'highlight.js/lib/languages/typescript'
+import html from 'highlight.js/lib/languages/xml'
+import { lowlight } from 'lowlight'
+
+lowlight.registerLanguage('html', html)
+lowlight.registerLanguage('css', css)
+lowlight.registerLanguage('js', js)
+lowlight.registerLanguage('ts', ts)
 
 export function Editor() {
   const editor = useEditor({
@@ -20,6 +32,10 @@ export function Editor() {
         placeholder: 'Digite alo...',
         emptyEditorClass:
           'before:content-[attr(data-placeholder)] before:text-gray-500 before:h-0 before:float-left before:pointer-events-none',
+      }),
+      CodeBlockLowlight.configure({
+        lowlight,
+        defaultLanguage: 'js',
       }),
     ],
     content: '<h1>Teste</h1>',
