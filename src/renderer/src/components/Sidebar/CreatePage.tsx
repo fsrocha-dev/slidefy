b2914093296fa0dc730a22776b5f13d336242b9a
Frank Rocha <contato.frankrocha@gmail.com>
2022-12-25 17:36:46 -0300

    chore: Add create document methods

--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -1,10 +1,35 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'phosphor-react'
+import { Document } from '@shared/types/ipc'
 
 export function CreatePage() {
+  const queryClient = useQueryClient()
+
+  const { isLoading: isCreatingNewDocument, mutateAsync: createDocument } =
+    useMutation(
+      async () => {
+        const response = await window.api.createDocument()
+
+        return response.data
+      },
+      {
+        onSuccess: (data) => {
+          queryClient.setQueryData<Document[]>(['documents'], (documents) => {
+            if (documents && documents.length >= 0) return [...documents, data]
+            return [data]
+          })
+        },
+      },
+    )
+
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-slidefy-600 hover:bg-slidefy-700 disabled:opacity-60">
+    <button
+      onClick={() => createDocument()}
+      disabled={isCreatingNewDocument}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-slidefy-600 hover:bg-slidefy-700 disabled:opacity-60"
+    >
       <Plus className="h-4 w-4" />
-      Create new page
+      New document
     </button>
   )
 }
