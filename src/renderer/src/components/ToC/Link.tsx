684cfa8938c10f0370ca825cdb3bba196e27ab29
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-19 13:26:01 -0300

    refactor: Remove ToC component

--- src/renderer/src/components/ToC/Link.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-interface ToCLinkProps {
-  children: ReactNode
-}
-
-export function ToCLink(props: ToCLinkProps) {
-  return <a href="#" className="hover:text-slidefy-50" {...props} />
-}
