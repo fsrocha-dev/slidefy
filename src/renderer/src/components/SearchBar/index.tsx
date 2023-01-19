1db0008c35a290fe6bcd6f99a0e8e7cedc7ad01e
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-19 13:30:27 -0300

    fix: Translations in components

--- src/renderer/src/components/SearchBar/index.tsx
+++ src/renderer/src/components/SearchBar/index.tsx
@@ -1,26 +1,43 @@
+import { useQuery } from '@tanstack/react-query'
 import { Command } from 'cmdk'
 import { File, MagnifyingGlass } from 'phosphor-react'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export function SearchBar() {
-  const [open, setOpen] = useState(false)
+interface SearchBarProps {
+  open: boolean
+  onOpenChange: (isOpen: boolean) => void
+}
+
+export function SearchBar({ open, onOpenChange }: SearchBarProps) {
+  const navigate = useNavigate()
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === 'k' && e.metaKey) {
-        setOpen((state) => !state)
+        onOpenChange(!open)
       }
     }
 
     document.addEventListener('keydown', down)
     return () => document.removeEventListener('keydown', down)
-  }, [setOpen])
+  }, [onOpenChange, open])
+
+  const { data } = useQuery(['documents'], async () => {
+    const response = await window.api.fetchDocuments()
+    return response.data
+  })
+
+  function handleOpenDocument(id: string) {
+    navigate(`/documents/${id}`)
+    onOpenChange(false)
+  }
 
   return (
     <Command.Dialog
       className="fixed top-24 left-1/2 -translate-x-1/2 w-[480px] max-w-full bg-slidefy-800 rounded-md shadow-2xl text-slidefy-100 border border-slidefy-600"
       open={open}
-      onOpenChange={setOpen}
+      onOpenChange={onOpenChange}
       label="Search"
     >
       <div className="flex items-center gap-2 border-b border-slidefy-700 p-4">
@@ -33,28 +50,21 @@ export function SearchBar() {
       </div>
       <Command.List className="py-2 max-h-48 scrollbar-thin scrollbar-thumb-slidefy-600 scrollbar-track-slidefy-800">
         <Command.Empty className="py-3 px-4 text-slidefy-200 text-sm">
-          Nenhum documento encontrado.
+          No documents found.
         </Command.Empty>
 
-        <Command.Item className="py-3 px-4 text-slidefy-50 text-sm flex items-center gap-2 hover:bg-slidefy-700 aria-selected:!bg-slidefy-600">
-          <File className="w-4 h-4" />
-          Untitled
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-slidefy-50 text-sm flex items-center gap-2 hover:bg-slidefy-700 aria-selected:!bg-slidefy-600">
-          <File className="w-4 h-4" />
-          Ignite
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-slidefy-50 text-sm flex items-center gap-2 hover:bg-slidefy-700 aria-selected:!bg-slidefy-600">
-          <File className="w-4 h-4" />
-          Discover
-        </Command.Item>
-
-        <Command.Item className="py-3 px-4 text-slidefy-50 text-sm flex items-center gap-2 hover:bg-slidefy-700 aria-selected:!bg-slidefy-600">
-          <File className="w-4 h-4" />
-          Rocketseat
-        </Command.Item>
+        {data?.map((document) => {
+          return (
+            <Command.Item
+              key={document.id}
+              onSelect={() => handleOpenDocument(document.id)}
+              className="py-3 px-4 text-slidefy-50 text-sm flex items-center gap-2 hover:bg-slidefy-700 aria-selected:!bg-slidefy-600"
+            >
+              <File className="w-4 h-4" />
+              {document.title}
+            </Command.Item>
+          )
+        })}
       </Command.List>
     </Command.Dialog>
   )
--- src/renderer/src/components/Sidebar/CreatePage.tsx
+++ src/renderer/src/components/Sidebar/CreatePage.tsx
@@ -46,7 +46,7 @@ export function CreatePage() {
       className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-slidefy-600 hover:bg-slidefy-700 disabled:opacity-60"
     >
       <Plus className="h-4 w-4" />
-      New document
+      New Presentation
     </button>
   )
 }
