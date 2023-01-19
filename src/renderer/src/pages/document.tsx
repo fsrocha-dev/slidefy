1cc74491d92f8f0535582ed58afdc1bd1d81c553
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-19 14:03:01 -0300

    chore: Remove ToC component from page documents

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,6 +1,5 @@
 import { useParams } from 'react-router-dom'
 import { Editor, OnContentUpdatedParams } from '../components/Editor'
-import { ToC } from '../components/ToC'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useMemo } from 'react'
 import { Document as IPCDocument } from '@shared/types/ipc'
@@ -53,19 +52,6 @@ export function Document() {
 
   return (
     <main className="flex-1 flex py-12 px-10 gap-8">
-      <aside className="hidden lg:block sticky top-0">
-        <span className="text-slidefy-100 font-semibold text-xs">
-          TABLE OF CONTENTS
-        </span>
-        <ToC.Root>
-          <ToC.Link>Back-end</ToC.Link>
-          <ToC.Section>
-            <ToC.Link>Banco de Dados</ToC.Link>
-            <ToC.Link>Authentication</ToC.Link>
-          </ToC.Section>
-        </ToC.Root>
-      </aside>
-
       <section className="flex-1 flex flex-col items-center">
         {!isFetching && data && (
           <Editor
