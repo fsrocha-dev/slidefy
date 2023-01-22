3fe0af801767c2ff47bf3053b0bb5c0eaff5ca39
Frank Rocha <contato.frankrocha@gmail.com>
2023-01-22 10:34:22 -0300

    chore: add app context to sidebar open on layout

--- src/renderer/src/layouts/default.tsx
+++ src/renderer/src/layouts/default.tsx
@@ -3,13 +3,15 @@ import * as Collapsible from '@radix-ui/react-collapsible'
 import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
-import { useState } from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../contexts/AppContext'
 
 export function Default() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const { setIsSidebarOpen, isSidebarOpen } = useContext(AppContext)
+
   return (
     <Collapsible.Root
-      defaultOpen
+      open={isSidebarOpen}
       onOpenChange={setIsSidebarOpen}
       className="h-screen w-screen text-slidefy-100 flex"
     >
